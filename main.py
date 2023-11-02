1f0c0a2959afa1aa5c786b87542d9e837e8d810c
mourad-boussiouf <92151677+mourad-boussiouf@users.noreply.github.com>
2023-11-02 10:47:26 +0100

    Update coherence heure de debut et fin

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from datetime import datetime, timedelta
 
 fake = Faker()
 
-
 import psycopg2
 
 conn = psycopg2.connect(
@@ -16,9 +15,8 @@ conn = psycopg2.connect(
 )
 cur = conn.cursor()
 
-# Generate 1000 users
-
-for _ in range(1000):
+# Generate 500 users
+for _ in range(500):
     username = fake.user_name()
     email = fake.email()
 
@@ -31,16 +29,26 @@ for _ in range(1000):
     for _ in range(5):
         time = fake.date_time_this_year()
         status = fake.boolean()
-        # affiliate clocks to all users
         cur.execute("""
             INSERT INTO clocks (time, status, user_id, inserted_at, updated_at)
             VALUES (%s, %s, %s, NOW(), NOW());
         """, (time, status, user_id))
 
     for _ in range(5):
-        start_time = fake.date_time_this_year()
-        end_time = start_time + timedelta(hours=random.randint(1, 8))
-        # affiliate working_times to all users
+        base_time = fake.date_time_this_year()
+
+        # Ensure start time is between 6am and 10am
+        start_time = base_time.replace(hour=random.randint(6, 10), minute=random.randint(0, 59),
+                                       second=random.randint(0, 59))
+
+        # Ensure end time is between 4pm and 8pm
+        end_time = base_time.replace(hour=random.randint(16, 20), minute=random.randint(0, 59),
+                                     second=random.randint(0, 59))
+
+        # Ensure end time is after start time
+        if end_time <= start_time:
+            end_time += timedelta(hours=random.randint(1, 4))
+
         cur.execute("""
             INSERT INTO working_times (start, "end", user_id, inserted_at, updated_at)
             VALUES (%s, %s, %s, NOW(), NOW());
