760717ae2e39b031ba73933bf343d036f83a8e84
mourad-boussiouf <92151677+mourad-boussiouf@users.noreply.github.com>
2023-11-02 13:34:44 +0100

    update coherence, les clocks sont set sur les memes dates
    que les workings times, les données sont forcément dans des dates passées et ne peuvent pas être setter dans le futur à des fins de tests.

--- main.py
+++ main.py
@@ -27,32 +27,33 @@ for _ in range(500):
     user_id = cur.fetchone()[0]
 
     for _ in range(5):
-        time = fake.date_time_this_year()
-        status = fake.boolean()
-        cur.execute("""
-            INSERT INTO clocks (time, status, user_id, inserted_at, updated_at)
-            VALUES (%s, %s, %s, NOW(), NOW());
-        """, (time, status, user_id))
-
-    for _ in range(5):
-        base_time = fake.date_time_this_year()
-
+        base_time = fake.date_time_this_decade()
+        
         # Ensure start time is between 6am and 10am
-        start_time = base_time.replace(hour=random.randint(6, 10), minute=random.randint(0, 59),
-                                       second=random.randint(0, 59))
+        start_time = base_time.replace(hour=random.randint(6, 10), minute=random.randint(0, 59), second=random.randint(0, 59))
 
-        # Ensure end time is between 4pm and 8pm
-        end_time = base_time.replace(hour=random.randint(16, 20), minute=random.randint(0, 59),
-                                     second=random.randint(0, 59))
-
-        # Ensure end time is after start time
+        # Ensure end time is between 4pm and 8pm and after start time
+        end_time = base_time.replace(hour=random.randint(16, 20), minute=random.randint(0, 59), second=random.randint(0, 59))
         if end_time <= start_time:
             end_time += timedelta(hours=random.randint(1, 4))
 
+        # Ensure dates are in the past
+        if start_time > datetime.now() or end_time > datetime.now():
+            continue
+
+        # Add working_times
         cur.execute("""
             INSERT INTO working_times (start, "end", user_id, inserted_at, updated_at)
             VALUES (%s, %s, %s, NOW(), NOW());
         """, (start_time, end_time, user_id))
+
+        # Add matching clocks
+        clock_time = start_time.replace(minute=random.randint(0, 59), second=random.randint(0, 59))
+        status = fake.boolean()
+        cur.execute("""
+            INSERT INTO clocks (time, status, user_id, inserted_at, updated_at)
+            VALUES (%s, %s, %s, NOW(), NOW());
+        """, (clock_time, status, user_id))
 
 conn.commit()
 cur.close()
